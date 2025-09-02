ba2042c933ec13aed29b924fd9ba4a4eb853ea8f
Maleesh Shanuka <sanuka.maleesh@gmail.com>
2025-09-02 13:45:00 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="grid g

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function About() {
   return (
     <section id="about" className="w-full py-12 md:py-24 lg:py-32 scroll-mt-16">
-      <div className="container px-4 md:px-6 pl-12 md:pl-20">
+      <div className="container px-4 md:px-6 pl-12 md:pl-20 pr-4 md:pr-8">
         <div className="grid gap-10 lg:gap-16 items-center md:grid-cols-2">
           <div className="space-y-4">
             <div className="space-y-2">
