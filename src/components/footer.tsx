41bc5db4be0f9dc1c079cc90b31bfad54d38846a
Maleesh Shanuka <sanuka.maleesh@gmail.com>
2025-09-03 13:26:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-sm

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="border-t">
-      <div className="container flex items-center justify-center h-16 max-w-7xl">
+      <div className="container flex items-center justify-center h-16 max-w-xl mx-auto">
         <p className="text-sm text-muted-foreground">
           &copy; maleeshshanuka - All rights reserved.
         </p>
