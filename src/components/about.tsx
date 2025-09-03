caed315cba0df12cad69713816bc9036eb95fb18
Maleesh Shanuka <sanuka.maleesh@gmail.com>
2025-09-03 13:52:59 +0000

    put " in the begin of Learn and " at the end of Create

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -15,9 +15,9 @@ export function About() {
               </h2>
             </div>
             <div className="text-5xl md:text-6xl lg:text-7xl font-extrabold py-4 tracking-tight space-y-4">
-              <div className="bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">Learn</div>
+              <div className="bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">"Learn</div>
               <div className="bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">Code</div>
-              <div className="bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">Create</div>
+              <div className="bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">Create"</div>
             </div>
           </div>
           <div className="flex items-center justify-center">
